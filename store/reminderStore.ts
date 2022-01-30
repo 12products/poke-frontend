b2ddc706accb64d8ae3fc20135a495fd638670d7
Anthony Morris <amorriscode@gmail.com>
2022-01-30 10:01:09 -0800

    Update store/reminderStore.ts

--- store/reminderStore.ts
+++ store/reminderStore.ts
@@ -5,6 +5,7 @@ export type ReminderState = {
   reminders: Reminder[]
   updateReminders: (reminders: Reminder[]) => void
 }
+
 export const useReminderStore = create<ReminderState>((set) => ({
   reminders: [],
   updateReminders: (reminders) => set({ reminders }),
